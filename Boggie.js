4daa9a4736ceec10a6a7466502b78f7ae04a4458
fjaved21 <53610549+fjaved21@users.noreply.github.com>
2020-09-09 00:15:56 +0530

    Add files via upload

---
+++ Boggie.js
@@ -0,0 +1,17 @@
+class Boggie{
+    constructor(x,y,w,h){
+var factors= {
+    isStatic:false
+}
+this.body= Bodies.rectangle(x,y,w,h,factors);
+this.w = w;
+this.h=h;
+this.image=loadImage("images/coach.png");
+World.add(myWorld,this.body);
+    }
+    show(){
+        imageMode(CENTER);
+        image(this.image,this.body.position.x,this.body.position.y,this.w,this.h);
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# 9th-sep-PHYSICS-ENGINE-STAGE-1
-includes boggie class, ground class, rock class and images
+# p5.play-boilerplate
+Boiler plate for p5.play
